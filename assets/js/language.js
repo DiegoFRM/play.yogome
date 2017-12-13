8c09a0f589821ca2fc36c603ac6cd366e873862e
hebersadurni <arsehase@live.com.mx>
2017-12-13 14:06:14 -0600

    LANGUAGE

--- assets/js/language.js
+++ assets/js/language.js
@@ -12,16 +12,26 @@ var languageArrayIndex = [
                 { 
                     id:1,
                     NAME:"mainMenu1",
-                    ES:"Jugar",
-                    EN:"Play",
-                    PT:"Jogar",
-                    ZH:"开始游戏",
-                    JA:"ゲーム開始",
-                    KO:"플레이"
+                    ES:"Yogodex",
+                    EN:"Yogodex",
+                    PT:"Yogodex",
+                    ZH:"Yogodex",
+                    JA:"Yogodex",
+                    KO:"요고덱스"
                 },
                 { 
                     id:2,
                     NAME:"mainMenu2",
+                    ES:"Juegos",
+                    EN:"Games",
+                    PT:"Jogos",
+                    ZH:"游戏",
+                    JA:"ゲーム",
+                    KO:"게임"
+                },
+                { 
+                    id:3,
+                    NAME:"mainMenu3",
                     ES:"Libros",
                     EN:"Books",
                     PT:"Livros",
@@ -30,38 +40,168 @@ var languageArrayIndex = [
                     KO:"도서관"
                 },
                 { 
-                    id:3,
-                    NAME:"mainMenu3",
-                    ES:"Episodios",
-                    EN:"Webisodes",
-                    PT:"Episódios",
-                    ZH:"网络视频短片",
-                    JA:"ウェブエピソード",
-                    KO:"웹피소드"
-                },
-                { 
                     id:4,
                     NAME:"mainMenu4",
-                    ES:"Episodios",
-                    EN:"Webisodes",
-                    PT:"Episódios",
-                    ZH:"网络视频短片",
-                    JA:"ウェブエピソード",
-                    KO:"웹피소드"
+                    ES:"Registro",
+                    EN:"Register",
+                    PT:"Recursos",
+                    ZH:"资源",
+                    JA:"資源",
+                    KO:"가입하기"
                 },
                 { 
                     id:5,
                     NAME:"mainMenu5",
-                    ES:"Episodios",
-                    EN:"Webisodes",
-                    PT:"Episódios",
-                    ZH:"网络视频短片",
-                    JA:"ウェブエピソード",
-                    KO:"웹피소드"
+                    ES:"Idiomas",
+                    EN:"Languages",
+                    PT:"Languages",
+                    ZH:"Languages",
+                    JA:"Languages",
+                    KO:"Languages"
                 },
                 { 
                     id:6,
                     NAME:"mainMenu6",
+                    ES:"Ingresar",
+                    EN:"Log In",
+                    PT:"Log In",
+                    ZH:"Log In",
+                    JA:"Log In",
+                    KO:"Log In"
+                },
+                { 
+                    id:7,
+                    NAME:"mainMenu7",
+                    ES:"Progreso",
+                    EN:"Progress",
+                    PT:"Progresso",
+                    ZH:"进度",
+                    JA:"進度",
+                    KO:"완성도"
+                },
+                { 
+                    id:8,
+                    NAME:"mainMenu8",
+                    ES:"Powercoins",
+                    EN:"Powercoins",
+                    PT:"Powercoins",
+                    ZH:"能量币",
+                    JA:"パワーコイン",
+                    KO:"파워 코인"
+                },
+                { 
+                    id:9,
+                    NAME:"mainMenu9",
+                    ES:"¡Premios!",
+                    EN:"Prizes",
+                    PT:"Prêmios",
+                    ZH:"奖品",
+                    JA:"賞品",
+                    KO:"보상!"
+                },
+                { 
+                    id:10,
+                    NAME:"mainMenu10",
+                    ES:"¡Juega!",
+                    EN:"Play",
+                    PT:"Jogar",
+                    ZH:"开始游戏",
+                    JA:"ゲーム開始",
+                    KO:"게임 시작!"
+                },
+//                { 
+//                    id:11,
+//                    NAME:"mainMenu11",
+//                    ES:"¿Quieres guardar tu avance? Dile a tus papás que agreguen su mail para que no pierdas tu progreso en el juego.",
+//                    EN:"Want to save your progress? Ask your parents to add their email so you don't lose your progress in the game.",
+//                    PT:"Quer salvar seu progresso? Peça para seus pais adicionarem o e-mail deles para você não perder seu progresso no jogo. ",
+//                    ZH:"希望保存进度吗？请让家长添加电子邮件地址，以便您能够保存游戏进度。",
+//                    JA:"進度を保存しますか？ゲームの進度を保存できるよう、保護者の方にEメールを登録するようお願いしてください。 ",
+//                    KO:"부모님이 이메일 주소를 입력하시면 게임을 저장해서 나중에도 이어서 할 수 있어요."
+//                },
+//                { 
+//                    id:12,
+//                    NAME:"mainMenu12",
+//                    ES:"¿Quieres seguir jugando? Llegaste al final de la prueba. Dile a tus papás que agreguen su mail para que la aventura continue.",
+//                    EN:"Want to keep playing? You're at the end of the test! Ask your parents to add their email so the adventure can continue.",
+//                    PT:"Quer continuar jogando? Você já está no final do teste! Peça para seus pais adicionarem o e-mail deles para a aventura continuar.",
+//                    ZH:"希望继续游戏吗？测试结束！请让家长添加电子邮件地址，以便您能继续探险",
+//                    JA:"ゲームを続けますか？テストの最後の問題です！ゲームを続けられるよう、保護者の方にEメールを登録するようお願いしてください。",
+//                    KO:"게임을 계속 하고 싶은가요? 이미 시험이 다 끝났어요! 부모님이 이메일 주소를 입력하시면 모험을 계속 이어갈 수 있어요."
+//                },
+                { 
+                    id:11,
+                    NAME:"mainMenu11",
+                    ES:"¿Cómo ser emprendedor?",
+                    EN:"How to Become an Entrepreneur",
+                    PT:"Como se tornar um empreendedor",
+                    ZH:"如何成为企业家",
+                    JA:"企業家になるには",
+                    KO:"사업가가 되는 방법"
+                },
+                { 
+                    id:12,
+                    NAME:"mainMenu12",
+                    ES:"Porqué las niñas deben ser ingenieras",
+                    EN:"Why Girls Should Be Engineers",
+                    PT:"Por que as garotas deveriam ser engenheiras",
+                    ZH:"为什么女孩应该当工程师",
+                    JA:"女の子がエンジニアに向いている理由",
+                    KO:"여성이 엔지니어가 되어야 하는 이유"
+                },
+                { 
+                    id:13,
+                    NAME:"mainMenu13",
+                    ES:"Idiomas",
+                    EN:"Languages",
+                    PT:"Languages",
+                    ZH:"Languages",
+                    JA:"Languages",
+                    KO:"Languages"
+                },
+                { 
+                    id:14,
+                    NAME:"mainMenu14",
+                    ES:"Ingresar",
+                    EN:"Log In",
+                    PT:"Log In",
+                    ZH:"Log In",
+                    JA:"Log In",
+                    KO:"Log In"
+                },
+                { 
+                    id:17,
+                    NAME:"mainMenu15",
+                    ES:"Progreso",
+                    EN:"Progress",
+                    PT:"Progresso",
+                    ZH:"进度",
+                    JA:"進度",
+                    KO:"완성도"
+                },
+                { 
+                    id:18,
+                    NAME:"mainMenu16",
+                    ES:"Powercoins",
+                    EN:"Powercoins",
+                    PT:"Powercoins",
+                    ZH:"能量币",
+                    JA:"パワーコイン",
+                    KO:"파워 코인"
+                },
+                { 
+                    id:19,
+                    NAME:"mainMenu17",
+                    ES:"¡Premios!",
+                    EN:"Prizes",
+                    PT:"Prêmios",
+                    ZH:"奖品",
+                    JA:"賞品",
+                    KO:"보상!"
+                },
+                { 
+                    id:20,
+                    NAME:"mainMenu18",
                     ES:"Episodios",
                     EN:"Webisodes",
                     PT:"Episódios",
@@ -70,14 +210,94 @@ var languageArrayIndex = [
                     KO:"웹피소드"
                 },
                 { 
-                    id:7,
+                    id:21,
+                    NAME:"mainMenu1",
+                    ES:"Yogodex",
+                    EN:"Yogodex",
+                    PT:"Yogodex",
+                    ZH:"Yogodex",
+                    JA:"Yogodex",
+                    KO:"요고덱스"
+                },
+                { 
+                    id:22,
+                    NAME:"mainMenu2",
+                    ES:"Juegos",
+                    EN:"Games",
+                    PT:"Jogos",
+                    ZH:"游戏",
+                    JA:"ゲーム",
+                    KO:"게임"
+                },
+                { 
+                    id:23,
+                    NAME:"mainMenu3",
+                    ES:"Libros",
+                    EN:"Books",
+                    PT:"Livros",
+                    ZH:"书籍",
+                    JA:"本",
+                    KO:"도서관"
+                },
+                { 
+                    id:24,
+                    NAME:"mainMenu4",
+                    ES:"Registro",
+                    EN:"Register",
+                    PT:"Recursos",
+                    ZH:"资源",
+                    JA:"資源",
+                    KO:"가입하기"
+                },
+                { 
+                    id:25,
+                    NAME:"mainMenu5",
+                    ES:"Idiomas",
+                    EN:"Languages",
+                    PT:"Languages",
+                    ZH:"Languages",
+                    JA:"Languages",
+                    KO:"Languages"
+                },
+                { 
+                    id:26,
+                    NAME:"mainMenu6",
+                    ES:"Ingresar",
+                    EN:"Log In",
+                    PT:"Log In",
+                    ZH:"Log In",
+                    JA:"Log In",
+                    KO:"Log In"
+                },
+                { 
+                    id:27,
                     NAME:"mainMenu7",
-                    ES:"Episodios",
-                    EN:"Webisodes",
-                    PT:"Episódios",
-                    ZH:"网络视频短片",
-                    JA:"ウェブエピソード",
-                    KO:"웹피소드"
+                    ES:"Progreso",
+                    EN:"Progress",
+                    PT:"Progresso",
+                    ZH:"进度",
+                    JA:"進度",
+                    KO:"완성도"
+                },
+                { 
+                    id:28,
+                    NAME:"mainMenu8",
+                    ES:"Powercoins",
+                    EN:"Powercoins",
+                    PT:"Powercoins",
+                    ZH:"能量币",
+                    JA:"パワーコイン",
+                    KO:"파워 코인"
+                },
+                { 
+                    id:29,
+                    NAME:"mainMenu9",
+                    ES:"¡Premios!",
+                    EN:"Prizes",
+                    PT:"Prêmios",
+                    ZH:"奖品",
+                    JA:"賞品",
+                    KO:"보상!"
                 }
     
     

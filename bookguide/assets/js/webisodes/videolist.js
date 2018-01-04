dcf4062c1875b5ddba976e4c46d9e398507a21a0
Diego Rojas Marcial <diego@yogome.com>
2018-01-03 22:53:31 -0600

    new section

---
+++ bookguide/assets/js/webisodes/videolist.js
@@ -0,0 +1,39 @@
+var videolist = 
+    [
+
+    {
+        videoID:1,
+        NAME_ES:"Invasión a Mathdrómeda",
+        NAME_EN:"Invasion on Mathdromeda",
+        url_ES:"TzOynq0Q4yQ",
+        url_EN:"JEIaVR13JE8"
+    },        
+    {
+        videoID:2,
+        NAME_ES:"¿Qué es un emprendedor?",
+        NAME_EN:"What is an Entrepreneur?",
+        url_ES:"VqOi-R1DuGc",
+        url_EN:'pKpuyMuMLCQ'
+    },
+    {
+        videoID:3,
+        NAME_ES:"¿Por qué las niñas deberían ser ingenieras?",
+        NAME_EN:"Why Girls Should Be Engineers",
+        url_ES:'hGZfPRUOQ7I',
+        url_EN:'OqiV7KrtL-M'
+    },
+    {
+        videoID:4,
+        NAME_ES:"Celebraciones de invierno alrededor del mundo",
+        NAME_EN:"Winter holidays around the world",
+        url_ES:'V7UXF7Xqlgc',
+        url_EN:'BMbOfiOWzZc'
+    },
+    {
+        videoID:5,
+        NAME_ES:"¿Quién celebra año nuevo primero?",
+        NAME_EN:"Who Celebrates New Year’s First?",
+        url_ES:'9pO-k0-bvNI',
+        url_EN:'BHPZbUfz2mo'
+    }
+    ]
